f516a7420e5ab5ed413de082486dc09a2fbe9f24
Esteban Uscanga <esteban@liquidlabs.de>
2015-11-29 22:20:35 +0100

    Returns always in error

--- src/picasaRequest.js
+++ src/picasaRequest.js
@@ -4,13 +4,13 @@ let request = require('request')
 
 function picasaRequest (method, requestOptions, callback) {
   request[method](requestOptions, (error, response, body) => {
-    if (error) callback(error)
+    if (error) return callback(error)
 
     if (response.statusCode == 403) return callback(new Error(body))
     if (response.statusCode != 200) {
       const unknownError = new Error('UNKNOWN_ERROR')
 
-      unknownError.statusCode = statusCode
+      unknownError.statusCode = response.statusCode
       unknownError.body = body
 
       return callback(unknownError)
