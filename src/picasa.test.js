fe8c887e841de97afd76381662a26da9e74e3534
Esteban Uscanga <esteban@liquidlabs.de>
2015-11-29 18:46:32 +0100

    Cleaning

--- src/picasa.test.js
+++ src/picasa.test.js
@@ -4,35 +4,97 @@ const chai   = require('chai')
 const sinon = require('sinon')
 const sinonChai = require('sinon-chai')
 
-const Picasa = require('./picasa')
-
 const expect = chai.expect
-
 chai.use(sinonChai)
+
+const Picasa = require('./picasa')
 
 describe('Picasa', () => {
   let picasa
+  let stub
 
   beforeEach(() => {
-    const clientSecret = 'client_secretABC'
-    const redirectURL = 'http://localhost'
     const clientId = 'apps.google.com'
+    const redirectURL = 'http://localhost'
+    const clientSecret = 'client_secretABC'
 
-    picasa = new Picasa(clientId, redirectURL)
+    picasa = new Picasa(clientId, redirectURL, clientSecret)
   })
 
+  afterEach(() => stub.restore())
+
   describe('getPhotos', () => {
-    it('returns an array of photos', (done) => {
-      const accessToken = 'ya29.OQKSGT56u8nnvO8XJiCmtNZPJqydwIlVrmbol0SDBQ-x3Y_SZZeVHrsfD1G2Rad4k0j0VQ'
+    describe('on status code different than 200', () => {
+      describe('on error 403', () => {
+        beforeEach(() => {
+          const body = 'weird error'
+          const response = { statusCode : 403 }
 
-      picasa.getPhotos(accessToken, (error, photos) => {
-        expect(error).to.be.equals(null)
+          stub = sinon.stub(picasa.request, 'get')
+          stub.callsArgWithAsync(1, null, response, body)
+        })
 
-        // https://lh3.googleusercontent.com/aaaaa/aaaaa/aaaaaaa/aaaaa/2015-11-23.jpg
-        expect(photos[0].src).to.contain('.googleusercontent.com')
-        expect(photos[0].type).to.be.equals('image/jpeg')
+        it('should return an error from the body', () => {
+          picasa.getPhotos('notValid', (error, photos) => {
+            expect(error.message).to.be.equals('weird error')
+            expect(photos).to.be.equals(null)
 
-        done()
+            done()
+          })
+        })
+      })
+    })
+
+    describe('on success', () => {
+      beforeEach(() => {
+        const body = {
+          "feed":{
+            "entry":[
+              {
+                "id":{
+                  "$t":"https://picasaweb.google.com/data/entry/user/11111/albumid/1111/photoid/11111"
+                },
+                "published":{
+                  "$t":"2015-11-28T07:13:31.000Z"
+                },
+                "updated":{
+                  "$t":"2015-11-28T07:22:35.478Z"
+                },
+                "app$edited":{
+                  "$t":"2015-11-28T07:22:35.478Z"
+                },
+                "title":{
+                  "$t":"IMG_0327.JPG"
+                },
+                "summary":{
+                  "$t":""
+                },
+                "content":{
+                  "type":"image/jpeg",
+                  "src":"https://lh3.googleusercontent.com/-1111111/1111/11111/1111/IMG_0327.JPG"
+                }
+              }
+            ]
+          }
+        }
+
+        const response = { statusCode : 200 }
+
+        stub = sinon.stub(picasa.request, 'get')
+        stub.callsArgWithAsync(1, null, response, JSON.stringify(body))
+      })
+
+      it('returns an array of photos', (done) => {
+        const accessToken = 'ya29.OwJqqa1Y2tivkkCWWvA8vt5ltKsdf9cDQ5IRNrTbIt-mcfr5xNj4dQu41K6hZa7lX9O-gw'
+
+        picasa.getPhotos(accessToken, (error, photos) => {
+          expect(error).to.be.equals(null)
+
+          expect(photos[0].src).to.contain('IMG_0327.JPG')
+          expect(photos[0].type).to.be.equals('image/jpeg')
+
+          done()
+        })
       })
     })
   })
@@ -45,15 +107,7 @@ describe('Picasa', () => {
   })
 
   describe('getAccessToken', () => {
-    let stub, picasa
-
     beforeEach(() => {
-      const clientSecret = 'client_secretABC'
-      const redirectURL = 'http://localhost'
-      const clientId = 'apps.google.com'
-
-      picasa = new Picasa(clientId, redirectURL, clientSecret)
-
       const body = {
         access_token : 'ya29.KwLDeXsw1jNAavZ8jEMFgikhDg_CnUX1oMr5RQUyeqTBf229YV4HzhhXvRgBBvFGqTqxdw',
         token_type   : 'Bearer',
@@ -63,8 +117,6 @@ describe('Picasa', () => {
       stub = sinon.stub(picasa.request, 'post')
       stub.callsArgWithAsync(1, null, {}, body)
     })
-
-    afterEach(() => stub.restore())
 
     it('returns access token response', (done) => {
       picasa.getAccessToken('4/DxoCTw8Rf3tQAAW94h6lK7ioEjnu6K8kEqVZ0d-cRA8', (error, accessToken) => {
