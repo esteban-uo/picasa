644359004c07a0e106735d06a0302489f1878a3b
Esteban Uscanga <esteban@liquidlabs.de>
2016-06-18 20:30:30 +0200

    Rename example to js

--- examples/config.example.js
+++ examples/config.example.js
@@ -1,5 +1,10 @@
-{
+// Get config here API Manager > Credentials
+// https://console.developers.google.com/home/dashboard
+
+const config = {
   "clientId"     : "fromGoogle.apps.googleusercontent.com",
   "clientSecret" : "fromGoogleclientSecret",
   "redirectURI"  : "http://your-app-url.com"
 }
+
+module.exports = config
