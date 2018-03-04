836571f32d2682d0e82067c0d28630357dd7bb76
Esteban Uscanga <esteban@m-tribes.com>
2018-03-04 20:47:37 +0100

    Put back callback in access token

--- examples/getAccessToken.js
+++ examples/getAccessToken.js
@@ -4,8 +4,10 @@ const Picasa = require('../.')
 const config = require('./config')
 
 // Second step
-const code = '4/etTOs_1FbU0vFqKFoX5IV9xe7z0Q5IBZICy0m1iGMNI'
+const code = '4/AAArsRhVP1HOfPmnh0dOqxMB7SfPAWE8roAI9JTHaKi6Q9qtCCPdhxh0HOwejQf7aEFNYMgLfaeaN95wjMVBWI0'
 
 const picasa = new Picasa(config)
 
-picasa.getAccessToken(config, code).then(console.log).catch(console.log)
+picasa.getAccessToken(config, code, (error, accessToken) => {
+  console.log(error, accessToken)
+})
